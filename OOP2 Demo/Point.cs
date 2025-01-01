4015170387e63b80ae2987b11a3c80f6e5683c73
Samer Zaki <Samer.Zaki22@svnu.edu.eg>
2025-01-01 22:46:44 +0200

    Add project files.

---
+++ OOP2 Demo/Point.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2_Demo
+{
+    internal struct Point
+    {
+        //Attributes
+        public int x;
+        public int y;
+
+        // in struct, compiler will always generate parametarless constructor
+        // this constructor will initialize each and every object
+        ///public Point()
+        ///{
+        ///    x = default;
+        ///    y = default;
+        ///}
+
+        //Constructor
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
---
+++ OOP2 Demo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.NetworkInformation;
+
+namespace OOP2_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point P1;
+            //Decleration
+
+
+            P1 = new Point(1, 2);
+            P1.y = 2;
+            Console.WriteLine(P1.x);
+            Console.WriteLine(P1.y);
+
+            Console.WriteLine(P1.ToString()); // Namespace.data type name
+        }
+    }
+}
